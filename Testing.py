3a6012d9aaf77a7bc431d781df9d40572bc7ec52
Raghu11999 <skraghu2097@gmail.com>
2024-08-04 22:27:24 +0530

    added message

---
+++ Testing.py
@@ -0,0 +1,109 @@
+from selenium import webdriver
+
+driver =webdriver.Chrome()
+# driver.get("https://omayo.blogspot.com/")
+# driver.find_element("id",'alert1').click()
+# element = driver.find_element("name" ,"q")
+# element.send_keys('ENTER')
+# driver.find_element("class name",'dropbtn').click()
+# element=driver.find_element(By.LINK_TEXT,"onlytestingblog").click()
+# original=driver.current_window_handle
+# driver.find_element(By.LINK_TEXT,("Open a popup window")).click()
+# windows=driver.window_handles
+
+"""for win in windows:
+    driver.switch_to.window(win)
+    if driver.title=="omayo (QAFox.com)":
+        a=True
+
+    else:
+        driver.close()
+    time.sleep(10)
+
+    driver.switch_to.window('original')
+    driver.close()"""
+
+# driver.find_element(By.PARTIAL_LINK_TEXT,"testwisely").click()
+# driver.find_element(By.CSS_SELECTOR,"input[value='Login']").click()
+# driver.find_element(By.XPATH,"//input[@value='Login']").click()
+# driver.find_element(By.ID,"textbox1").clear()
+# driver.find_element(By.CSS_SELECTOR,"div[id='HTML11'] textarea").clear()
+# driver.get("https://tutorialsninja.com/demo/index.php?route=product/category&path=20_27")
+# driver.maximize_window()
+# driver.close()
+# page_title=driver.title
+# print("the title of the current page is "+ page_title)
+# cur_page=driver.current_url
+# print("the current page is "+ cur_page)
+
+# text_area_filed_two=driver.find_element(By.CSS_SELECTOR,"div[id='HTML11'] textarea").text
+# print(text_area_filed_two)
+# get_label=driver.find_element(By.ID,"alert1").get_attribute("value")
+# print(get_label)
+# url_of_hyperlink=driver.find_element(By.LINK_TEXT,"compendiumdev").get_attribute("href")
+# print(url_of_hyperlink)
+# driver.find_element(By.LINK_TEXT,"Open a popup window").click()
+# driver.quit()
+# buttonDisplay=driver.find_element(By.ID,"hbutton").is_displayed()
+# if buttonDisplay:
+#     rint("Button display on the page")
+# else:
+#     print("Button display not on the page")
+# ButtonStatus=driver.find_element(By.ID,'but1').is_enabled()
+# if ButtonStatus:
+#     print("Button is in Enabled")
+# else:
+#     print("Button  is not Enabled")
+# checkboxstate=driver.find_element(By.ID,'checkbox2').is_selected()
+# if checkboxstate:
+#     print("checkbox filed is selected")
+# else:
+#     print("checkbox filed is not selected state")
+#     driver.close()
+# State=driver.find_element(By.NAME,"vehicle").is_selected()
+# if State:
+#     print("Radio button filed is selected")
+# else:
+#     print("radio button filed not selected state")
+# driver.find_element(By.CLASS_NAME,"dropbtn").click()
+# time.sleep(10)
+# driver.find_element(By.LINK_TEXT,"
+# time.sleep(5)
+# driver.get("https://tutorialsninja.com/")
+# driver.back()
+# time.sleep(5)
+# driver.forward()
+# time.sleep(5)
+# driver.get("https://testpages.herokuapp.com/basic_web_page.html")
+# print(driver.page_source)
+# driver.close()
+# driver.get("https://omayo.blogspot.com/")
+# driver.set_window_size(1500,1400)
+# emailAddress=driver.find_element(By.NAME,'email')
+# password=driver.find_element(By.CLASS_NAME,'')
+# emailAddress.send_keys("skraghu2097@gmail.com")
+# password.send_keys("Rag$1999")
+# time.sleep(5)
+# password.submit()
+driver.get("https://tutorialsninja.com/demo/")
+driver.save_screenshot("screenshort tutorialsninja.png")
+# driver.get_screenshot_as_file("screenshort\\Tutorialsninja.png")
+
+# tagsearch=driver.find_element(By.NAME,"search").tag_name
+# print(# tagsearch)
+# driver.close()
+# time.sleep(5)
+# serachsize =driver.find_element(By.NAME,"search")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ program.py
@@ -0,0 +1,14 @@
+# a="Hello World!"
+# char= a[0]
+# print("first word " + char)
+# char= a[-1]
+# print("last character "+ char)
+c="Hello"
+d="world"
+combined=c +" "+ d
+print(combined)
+
+# reverse
+a=[1,2,3,4,5,6]
+reverse=a[::-1]
+print(reverse)
